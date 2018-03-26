814a4191b04464abfe9db0e18d2db8ed356a7552
HEARI-NOTEBOOK <hasanemreari@gmail.com>
2018-03-26 15:34:29 +0300

    AccountTest test completed.

--- src/test/java/com/aric/samples/account/model/AccountTest.java
+++ src/test/java/com/aric/samples/account/model/AccountTest.java
@@ -17,16 +17,56 @@ public class AccountTest {
 
 
     @Test
-    public void testGetUserId() {
+    public void testGetUserId()throws Exception {
         Assert.fail("Not implemented yet!");
     }
 
     @Test
-    public void testGetUserBalance() {
+    public void testGetUserBalance() throws Exception{
         account = new Account();
         account.setBalance(BALANCE);
         Assert.assertEquals(account.getBalance(), BALANCE, 0.0001);
     }
-
-
+    @Test
+    public void testGetUserOwnerTckn() {
+        account = new Account();
+        account.setOwnerTckn(OWNER_TCKN);
+        Assert.assertEquals(account.getOwnerTckn(), OWNER_TCKN);
+    }
+    @Test
+    public void testGetUserOwnerFirstName() {
+        account = new Account();
+        account.setOwnerFirstName(OWNER_FIRST_NAME);
+        Assert.assertEquals(account.getOwnerFirstName(), OWNER_FIRST_NAME);
+    }
+    @Test
+    public void testGetUserOwnerLastName() {
+        account = new Account();
+        account.setOwnerLastName(OWNER_LAST_NAME);
+        Assert.assertEquals(account.getOwnerLastName(), OWNER_LAST_NAME);
+    }
+    @Test
+    public void testSetUserBalance() {
+        account = new Account();
+        account.setBalance(BALANCE);
+        Assert.assertEquals(account.getBalance(), BALANCE, 0.0001);
+    }
+    @Test
+    public void testSetUserOwnerTckn() {
+        account = new Account();
+        account.setOwnerTckn(OWNER_TCKN);
+        Assert.assertEquals(account.getOwnerTckn(), OWNER_TCKN);
+    }
+    @Test
+    public void testSetUserOwnerFirstName() {
+        account = new Account();
+        account.setOwnerFirstName(OWNER_FIRST_NAME);
+        Assert.assertEquals(account.getOwnerFirstName(), OWNER_FIRST_NAME);
+    }
+    @Test
+    public void testSetUserOwnerLastName() {
+        account = new Account();
+        account.setOwnerLastName(OWNER_LAST_NAME);
+        Assert.assertEquals(account.getOwnerLastName(), OWNER_LAST_NAME);
+    }
 }
